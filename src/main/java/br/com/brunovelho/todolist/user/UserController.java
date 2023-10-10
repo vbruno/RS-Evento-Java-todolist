0ad4e695e399e059a5d581e94cba9a47d6d0c5d3
Bruno S Velho <bruno.velho@gmail.com>
2023-10-10 10:01:50 -0300

    aula 02

--- src/main/java/br/com/brunovelho/todolist/user/UserController.java
+++ src/main/java/br/com/brunovelho/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.brunovelho.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.username);
-        System.out.println(userModel.name);
-        System.out.println(userModel.password);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(400).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
